daa9a48b20b88e50f9ec3d00bebca22aa9f1f249
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-03-28 21:28:01 -0400

    Run with Python 3.10 using asdf

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
 )
