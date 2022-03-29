55353175e07a0718450cd94e4d3eb378f6b40190
Zeph Grunschlag <zeph@algorand.com>
2022-03-28 22:29:53 -0400

    try packge_dir instead of packages

--- setup.py
+++ setup.py
@@ -14,12 +14,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/algorand/graviton",
-    packages=setuptools.find_packages(),
+    # packages=setuptools.find_packages(),
+    package_dir={"": "."},
     install_requires=["py-algorand-sdk", "tabulate==0.8.9"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_data={"pyteal": ["*.pyi"]},
     python_requires=">=3.8",
 )
