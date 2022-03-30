be41d271f5d08b1132cd8ff9ecf7d2608ab88501
Zeph Grunschlag <zeph@algorand.com>
2022-03-30 00:27:10 -0400

    delete commented out code

--- tests/clients.py
+++ tests/clients.py
@@ -1,8 +1,5 @@
 from algosdk.v2client.algod import AlgodClient
 
-# from algosdk.kmd import KMDClient
-# from algosdk.v2client.indexer import IndexerClient
-
 DEVNET_TOKEN = "a" * 64
 ALGOD_PORT = 4001
 
