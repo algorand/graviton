482258cc0a8a8efb071502b3f5e1768bee295702
Zeph Grunschlag <zeph@algorand.com>
2022-05-16 22:43:11 -0500

    0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="graviton",
-    version="0.3.1",
+    version="0.3.0",
     url="https://github.com/algorand/graviton",
     description="verify your TEAL program by experiment and observation",
     long_description=long_description,
